bf6f3d8f4da43dbc6f26011a8d23463628332755
sergej <neodin2015@gmail.com>
2024-08-29 02:09:22 +0500

    feat: change private routes wrapper

--- src/app/router/PrivateWrapper.tsx
+++ src/app/router/PrivateWrapper.tsx
@@ -1,7 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { ROUTES } from '@/shared';
+import { useAuth } from '@/app/providers/AuthProviders';
 
 export const PrivateWrapper = () => {
-	const isAuthenticated = false
+	const { isAuthenticated } = useAuth();
 	return isAuthenticated ? <Outlet /> : <Navigate to={ROUTES.USER.LOGIN} />;
 };
