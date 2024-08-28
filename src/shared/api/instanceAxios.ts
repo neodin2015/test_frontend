6437aa732088d3f43df6bf1d48db805bdae1e7b0
sergej <neodin2015@gmail.com>
2024-08-29 02:22:06 +0500

    refactor: replace hardcode baseURL on instanceAxios

--- src/shared/api/instanceAxios.ts
+++ src/shared/api/instanceAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instanceAxios = axios.create({
-	baseURL: 'http://localhost:3500/',
+	baseURL: process.env.REACT_APP_API_URL,
 });
 
 instanceAxios.interceptors.request.use(config => {
